0c804780637275c042eba9af60fc30f8207d1a6c
Trent-Duncan <duncantr131@northwested.org>
2024-09-18 13:56:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#Trent Duncan
+#9/18/24
+#IPO Intro
+
+num1 =float( input('Enter a number: (example: 21)\n'))
+num2 =float(input('Enter another number: (example: 69\n'))
+
+total = num1 + num2
+
+print(f'First number enterd was: {num1}')
+print(f'Second number enterd was: {num2}')
+print()
+print(f' The sum of {num1} and {num2} is: {total}')
